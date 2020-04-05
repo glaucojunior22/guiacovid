0d48e6ba218c391509e09ac8813163b4d3e7b3b4
Glauco Junior <glaucojunior22@gmail.com>
2020-04-05 09:58:36 -0300

    Change serializer to return a list of urls.

--- api/serializers.py
+++ api/serializers.py
@@ -4,14 +4,28 @@ from portal.models import Source, Category, Tag
 class SourceSerializer(serializers.ModelSerializer):
     # tags = serializers.StringRelatedField(many=True)
     # category = serializers.StringRelatedField()
+    urls = serializers.SerializerMethodField()
     
     class Meta:
         model = Source
         # fields = '__all__'
-        fields = ['id', 'title', 'description', 'url', 'image', 'category',
-                  'tags', 'facebook', 'instagram', 'twitter', 'whatsapp',
-                  'play_store', 'app_store']
+        # fields = ['id', 'title', 'description', 'url', 'image', 'category',
+        #           'tags', 'facebook', 'instagram', 'twitter', 'whatsapp',
+        #           'play_store', 'app_store']
+        fields = ['id', 'title', 'description', 'image', 'category', 'tags',
+                  'urls']
 
+
+    def get_urls(self, obj):
+        print(type(obj))
+        urls = []
+        links = ['site', 'instagram', 'facebook', 'twitter', 'whatsapp',
+                 'play_store', 'app_store']
+        for link in links:
+            if getattr(obj, link) is not None:
+                # urls[link] = getattr(obj, link, '')
+                urls.append({'app': link, 'url': getattr(obj, link, '')})
+        return urls
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
